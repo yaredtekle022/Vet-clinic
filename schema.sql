3355cfbb8881177e76a9bb3eb677fec3ed6678aa
yaredtekle022 <yaredtekle022@gmail.com>
2023-08-29 12:55:04 +0300

    Create owners and species table

--- schema.sql
+++ schema.sql
@@ -10,3 +10,24 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD COLUMN species VARCHAR(255);
+
+CREATE TABLE owners (
+    id serial PRIMARY KEY,
+    full_name varchar(255),
+    age integer
+);
+
+CREATE TABLE species (
+    id serial PRIMARY KEY,
+    name varchar(255)
+);
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+
+ALTER TABLE animals
+ADD COLUMN species_id integer REFERENCES species(id);
+
+ALTER TABLE animals
+ADD COLUMN owner_id integer REFERENCES owners(id);
